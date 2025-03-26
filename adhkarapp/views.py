dc7e07aaebae1383bbba78aea44f99f93da35bb3
Ishak <ishak.ariif@gmail.com>
2025-03-26 16:18:19 +0000

    Add Bootstrap to base template

--- adhkarapp/views.py
+++ adhkarapp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the adhkar index.")
 # Create your views here.
 
 def homepage(request):
